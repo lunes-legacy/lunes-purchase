3e8d3e8d2ad89e4a684a6a062e39a4732d79110e
Daniel Laurindo <dslaurindo@gmail.com>
2018-02-25 16:20:47 -0300

    FIx URL

--- dist/app.js
+++ dist/app.js
@@ -53185,6 +53185,7 @@
 	    this.showContainerCoins = false;
 	    this.balanceCoins = {};
 	    this.currentPhase = [];
+	    this.buyHistoryUser = {};
 	    this.valueToDeposit = '0.00000000';
 	    this.valueToReceive = '0.00000000';
 	    this.bonusAmountFinal = '0.00000000';
@@ -53223,27 +53224,28 @@
 	    this.obtainPhase().catch(function (error) {
 	      console.log(error);
 	    });
+	    /*this.getBuyHistory().catch(error => {
+	      console.log(error);
+	    });*/
 	    this.showLoading(true);
 	  }
 	
-	  BuyController.prototype.showDepositWalletAddressQRCode = function () {
+	  BuyController.prototype.getBuyHistory = function () {
 	    var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-	      var a;
+	      var buyHistory;
 	      return regeneratorRuntime.wrap(function _callee$(_context) {
 	        while (1) {
 	          switch (_context.prev = _context.next) {
 	            case 0:
 	              _context.next = 2;
-	              return this.HttpService.showDepositWalletAddressQRCode(this.currentUser, this.currentCoinSelected);
+	              return this.HttpService.buyHistory(this.currentUser.email, this.currentUser.accessToken);
 	
 	            case 2:
-	              a = _context.sent;
+	              buyHistory = _context.sent;
 	
-	              this.currentQRCode = JSON.parse(JSON.stringify(a));
-	              this.getCurrentBalanceUser(this.currentCoinSelected.name, this.currentQRCode.address, this.currentUser);
-	              //this.balance = this.HttpService.getBalance(this.currentCoinSelected.name, this.currentQRCode.address, this.currentUser);
+	              this.buyHistoryUser = buyHistory;
 	
-	            case 5:
+	            case 4:
 	            case 'end':
 	              return _context.stop();
 	          }
@@ -53251,19 +53253,30 @@
 	      }, _callee, this);
 	    }));
 	
-	    function showDepositWalletAddressQRCode() {
+	    function getBuyHistory() {
 	      return _ref.apply(this, arguments);
 	    }
 	
-	    return showDepositWalletAddressQRCode;
+	    return getBuyHistory;
 	  }();
 	
-	  BuyController.prototype.doBuy = function () {
+	  BuyController.prototype.showDepositWalletAddressQRCode = function () {
 	    var _ref2 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
+	      var a;
 	      return regeneratorRuntime.wrap(function _callee2$(_context2) {
 	        while (1) {
 	          switch (_context2.prev = _context2.next) {
 	            case 0:
+	              _context2.next = 2;
+	              return this.HttpService.showDepositWalletAddressQRCode(this.currentUser, this.currentCoinSelected);
+	
+	            case 2:
+	              a = _context2.sent;
+	
+	              this.currentQRCode = JSON.parse(JSON.stringify(a));
+	              this.getCurrentBalanceUser(this.currentCoinSelected.name, this.currentQRCode.address, this.currentUser);
+	
+	            case 5:
 	            case 'end':
 	              return _context2.stop();
 	          }
@@ -53271,8 +53284,28 @@
 	      }, _callee2, this);
 	    }));
 	
-	    function doBuy() {
+	    function showDepositWalletAddressQRCode() {
 	      return _ref2.apply(this, arguments);
+	    }
+	
+	    return showDepositWalletAddressQRCode;
+	  }();
+	
+	  BuyController.prototype.doBuy = function () {
+	    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3() {
+	      return regeneratorRuntime.wrap(function _callee3$(_context3) {
+	        while (1) {
+	          switch (_context3.prev = _context3.next) {
+	            case 0:
+	            case 'end':
+	              return _context3.stop();
+	          }
+	        }
+	      }, _callee3, this);
+	    }));
+	
+	    function doBuy() {
+	      return _ref3.apply(this, arguments);
 	    }
 	
 	    return doBuy;
@@ -53289,18 +53322,18 @@
 	  };
 	
 	  BuyController.prototype.obtainPhase = function () {
-	    var _ref3 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee3() {
+	    var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4() {
 	      var phase;
-	      return regeneratorRuntime.wrap(function _callee3$(_context3) {
+	      return regeneratorRuntime.wrap(function _callee4$(_context4) {
 	        while (1) {
-	          switch (_context3.prev = _context3.next) {
+	          switch (_context4.prev = _context4.next) {
 	            case 0:
 	              console.log('obtainPhase');
-	              _context3.prev = 1;
+	              _context4.prev = 1;
 	              phase = void 0;
 	
 	              if (!localStorage.getItem('lunes.phase')) {
-	                _context3.next = 11;
+	                _context4.next = 11;
 	                break;
 	              }
 	
@@ -53317,16 +53350,16 @@
 	              }
 	
 	              this.showLoading(false);
-	              return _context3.abrupt('return');
+	              return _context4.abrupt('return');
 	
 	            case 11:
-	              _context3.next = 13;
+	              _context4.next = 13;
 	              return this.HttpService.obtainPhase().catch(function (error) {
 	                alert('Erro ao tentar recuperar dados da fase da ICO');
 	              });
 	
 	            case 13:
-	              this.currentPhase = _context3.sent;
+	              this.currentPhase = _context4.sent;
 	
 	              console.log(this.currentPhase);
 	
@@ -53345,61 +53378,31 @@
 	                localStorage.setItem('lunes.phase', JSON.stringify(this.currentPhase));
 	              }
 	              this.showLoading(false);
-	              _context3.next = 26;
+	              _context4.next = 26;
 	              break;
 	
 	            case 23:
-	              _context3.prev = 23;
-	              _context3.t0 = _context3['catch'](1);
+	              _context4.prev = 23;
+	              _context4.t0 = _context4['catch'](1);
 	
-	              console.log(_context3.t0);
+	              console.log(_context4.t0);
 	
 	            case 26:
 	            case 'end':
-	              return _context3.stop();
+	              return _context4.stop();
 	          }
 	        }
-	      }, _callee3, this, [[1, 23]]);
+	      }, _callee4, this, [[1, 23]]);
 	    }));
 	
 	    function obtainPhase() {
-	      return _ref3.apply(this, arguments);
+	      return _ref4.apply(this, arguments);
 	    }
 	
 	    return obtainPhase;
 	  }();
 	
 	  BuyController.prototype.getBalanceCoin = function () {
-	    var _ref4 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4(coin) {
-	      var balance;
-	      return regeneratorRuntime.wrap(function _callee4$(_context4) {
-	        while (1) {
-	          switch (_context4.prev = _context4.next) {
-	            case 0:
-	              _context4.next = 2;
-	              return this.HttpService.getBitcoinBalance(coin);
-	
-	            case 2:
-	              balance = _context4.sent;
-	
-	              this.balanceCoins[coin] = { balance: balance };
-	
-	            case 4:
-	            case 'end':
-	              return _context4.stop();
-	          }
-	        }
-	      }, _callee4, this);
-	    }));
-	
-	    function getBalanceCoin(_x) {
-	      return _ref4.apply(this, arguments);
-	    }
-	
-	    return getBalanceCoin;
-	  }();
-	
-	  BuyController.prototype.getBalanceCoinETH = function () {
 	    var _ref5 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee5(coin) {
 	      var balance;
 	      return regeneratorRuntime.wrap(function _callee5$(_context5) {
@@ -53407,7 +53410,7 @@
 	          switch (_context5.prev = _context5.next) {
 	            case 0:
 	              _context5.next = 2;
-	              return this.HttpService.getBalanceCoinETH(coin);
+	              return this.HttpService.getBitcoinBalance(coin);
 	
 	            case 2:
 	              balance = _context5.sent;
@@ -53422,22 +53425,22 @@
 	      }, _callee5, this);
 	    }));
 	
-	    function getBalanceCoinETH(_x2) {
+	    function getBalanceCoin(_x) {
 	      return _ref5.apply(this, arguments);
 	    }
 	
-	    return getBalanceCoinETH;
+	    return getBalanceCoin;
 	  }();
 	
-	  BuyController.prototype.getBalanceLunes = function () {
-	    var _ref6 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee6(coin, currentUser) {
+	  BuyController.prototype.getBalanceCoinETH = function () {
+	    var _ref6 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee6(coin) {
 	      var balance;
 	      return regeneratorRuntime.wrap(function _callee6$(_context6) {
 	        while (1) {
 	          switch (_context6.prev = _context6.next) {
 	            case 0:
 	              _context6.next = 2;
-	              return this.HttpService.getBalanceLunes(coin, currentUser);
+	              return this.HttpService.getBalanceCoinETH(coin);
 	
 	            case 2:
 	              balance = _context6.sent;
@@ -53452,8 +53455,38 @@
 	      }, _callee6, this);
 	    }));
 	
-	    function getBalanceLunes(_x3, _x4) {
+	    function getBalanceCoinETH(_x2) {
 	      return _ref6.apply(this, arguments);
+	    }
+	
+	    return getBalanceCoinETH;
+	  }();
+	
+	  BuyController.prototype.getBalanceLunes = function () {
+	    var _ref7 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee7(coin, currentUser) {
+	      var balance;
+	      return regeneratorRuntime.wrap(function _callee7$(_context7) {
+	        while (1) {
+	          switch (_context7.prev = _context7.next) {
+	            case 0:
+	              _context7.next = 2;
+	              return this.HttpService.getBalanceLunes(coin, currentUser);
+	
+	            case 2:
+	              balance = _context7.sent;
+	
+	              this.balanceCoins[coin] = { balance: balance };
+	
+	            case 4:
+	            case 'end':
+	              return _context7.stop();
+	          }
+	        }
+	      }, _callee7, this);
+	    }));
+	
+	    function getBalanceLunes(_x3, _x4) {
+	      return _ref7.apply(this, arguments);
 	    }
 	
 	    return getBalanceLunes;
@@ -53579,20 +53612,20 @@
 	  };
 	
 	  BuyController.prototype.getCurrentBalanceUser = function () {
-	    var _ref7 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee7(coin, address, currentUser) {
+	    var _ref8 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee8(coin, address, currentUser) {
 	      var _this = this;
 	
 	      var balance;
-	      return regeneratorRuntime.wrap(function _callee7$(_context7) {
+	      return regeneratorRuntime.wrap(function _callee8$(_context8) {
 	        while (1) {
-	          switch (_context7.prev = _context7.next) {
+	          switch (_context8.prev = _context8.next) {
 	            case 0:
 	              console.log("");
-	              _context7.next = 3;
+	              _context8.next = 3;
 	              return this.HttpService.getBalance(coin, address, currentUser);
 	
 	            case 3:
-	              balance = _context7.sent;
+	              balance = _context8.sent;
 	
 	              this.$timeout(function () {
 	                _this.balanceUser = balance;
@@ -53600,14 +53633,14 @@
 	
 	            case 5:
 	            case 'end':
-	              return _context7.stop();
+	              return _context8.stop();
 	          }
 	        }
-	      }, _callee7, this);
+	      }, _callee8, this);
 	    }));
 	
 	    function getCurrentBalanceUser(_x5, _x6, _x7) {
-	      return _ref7.apply(this, arguments);
+	      return _ref8.apply(this, arguments);
 	    }
 	
 	    return getCurrentBalanceUser;
@@ -82040,7 +82073,7 @@
 	  WELCOME: 'Welcome',
 	  BEGIN_YOUR_REGISTRATION: 'Begin your registration',
 	  AGREE: 'Agree',
-	  DONT_HAVE_ACCOUNT: 'D\'ont have an account?',
+	  DONT_HAVE_ACCOUNT: 'Don\'t have an account?',
 	  SIGNUP: 'Signup',
 	  RESET_PASSWORD: 'Reset your password',
 	  WHITELIST: 'Whitelist',
@@ -82478,20 +82511,21 @@
 	    return getBalanceCoinETH;
 	  }();
 	
-	  HttpService.prototype.getBalance = function () {
-	    var _ref8 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee8(coin, address, currentUser) {
-	      var underCoin, balance;
+	  HttpService.prototype.buyHistory = function () {
+	    var _ref8 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee8(email, accessToken) {
+	      var a;
 	      return regeneratorRuntime.wrap(function _callee8$(_context8) {
 	        while (1) {
 	          switch (_context8.prev = _context8.next) {
 	            case 0:
-	              underCoin = coin.toLowerCase();
-	              _context8.next = 3;
-	              return _lunesLib2.default.coins.getBalance({ address: address, coin: underCoin, testnet: false }, currentUser.accessToken);
+	              _context8.next = 2;
+	              return _lunesLib2.default.coins.buyHistory(email, accessToken);
 	
-	            case 3:
-	              balance = _context8.sent;
-	              return _context8.abrupt('return', balance && balance.data ? balance.data : {});
+	            case 2:
+	              a = _context8.sent;
+	
+	              console.log(a);
+	              return _context8.abrupt('return', a);
 	
 	            case 5:
 	            case 'end':
@@ -82501,19 +82535,49 @@
 	      }, _callee8, this);
 	    }));
 	
-	    function getBalance(_x9, _x10, _x11) {
+	    function buyHistory(_x9, _x10) {
 	      return _ref8.apply(this, arguments);
+	    }
+	
+	    return buyHistory;
+	  }();
+	
+	  HttpService.prototype.getBalance = function () {
+	    var _ref9 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee9(coin, address, currentUser) {
+	      var underCoin, balance;
+	      return regeneratorRuntime.wrap(function _callee9$(_context9) {
+	        while (1) {
+	          switch (_context9.prev = _context9.next) {
+	            case 0:
+	              underCoin = coin.toLowerCase();
+	              _context9.next = 3;
+	              return _lunesLib2.default.coins.getBalance({ address: address, coin: underCoin, testnet: false }, currentUser.accessToken);
+	
+	            case 3:
+	              balance = _context9.sent;
+	              return _context9.abrupt('return', balance && balance.data ? balance.data : {});
+	
+	            case 5:
+	            case 'end':
+	              return _context9.stop();
+	          }
+	        }
+	      }, _callee9, this);
+	    }));
+	
+	    function getBalance(_x11, _x12, _x13) {
+	      return _ref9.apply(this, arguments);
 	    }
 	
 	    return getBalance;
 	  }();
 	
 	  HttpService.prototype.getBitcoinBalance = function () {
-	    var _ref9 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee9(coin) {
+	    var _ref10 = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee10(coin) {
 	      var toSymbol, queryObj, tsym, priceData, currentPrice, displayPrice, ticker;
-	      return regeneratorRuntime.wrap(function _callee9$(_context9) {
+	      return regeneratorRuntime.wrap(function _callee10$(_context10) {
 	        while (1) {
-	          switch (_context9.prev = _context9.next) {
+	          switch (_context10.prev = _context10.next) {
 	            case 0:
 	              toSymbol = 'USD';
 	              queryObj = {
@@ -82522,11 +82586,11 @@
 	                exchange: this.$translate.instant('CURRENCY_EXCHANGE')
 	              };
 	              tsym = _cccStreamerUtilities2.default.STATIC.CURRENCY.getSymbol(toSymbol);
-	              _context9.next = 5;
+	              _context10.next = 5;
 	              return _lunesLib2.default.coins.getPrice(queryObj);
 	
 	            case 5:
-	              priceData = _context9.sent;
+	              priceData = _context10.sent;
 	              currentPrice = _cccStreamerUtilities2.default.convertValueToDisplay(tsym, priceData[toSymbol]);
 	              displayPrice = '1 ' + coin + ' | ' + currentPrice;
 	              ticker = {
@@ -82538,18 +82602,18 @@
 	                CHANGE: 'up',
 	                COIN: coin
 	              };
-	              return _context9.abrupt('return', ticker);
+	              return _context10.abrupt('return', ticker);
 	
 	            case 10:
 	            case 'end':
-	              return _context9.stop();
+	              return _context10.stop();
 	          }
 	        }
-	      }, _callee9, this);
+	      }, _callee10, this);
 	    }));
 	
-	    function getBitcoinBalance(_x12) {
-	      return _ref9.apply(this, arguments);
+	    function getBitcoinBalance(_x14) {
+	      return _ref10.apply(this, arguments);
 	    }
 	
 	    return getBitcoinBalance;
