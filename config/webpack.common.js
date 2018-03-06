be21298dada30dc15e5ff530ea9b5091167b0277
Daniel Laurindo <dslaurindo@gmail.com>
2018-03-05 21:12:10 -0300

    add version to app.js

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, '..', 'dist'),
-    filename: `[name].js?${new Date().getTime()}`
+    filename: `[name].js?v=${new Date().getTime()}`
   },
   plugins: [
     new HtmlWebpackPlugin({
